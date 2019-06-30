5e51be7e117e9efd8774a998c26d6d0274494689
Alan Krajina <alankrajina88@gmail.com>
2019-06-30 13:57:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
   create_table :costume_stores do |t|
       t.string :name
-      t.text :location
+      t.string :location
       t.integer :price
       t.text :size
       t.string :image_url
