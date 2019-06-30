73452a624728ef48c14e07e943a121f507aa41ef
Alan Krajina <alankrajina88@gmail.com>
2019-06-30 13:56:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+  create_table :costume_stores do |t|
+      t.string :name
+      t.integer :price
+      t.text :size
+      t.string :image_url
+      t.timestamps 
+    end
+  end  
+  
+end
